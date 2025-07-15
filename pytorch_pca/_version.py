db840bf6df0ac059815e95734ebb787126364c2b
Ricardo Yanzon <ricayanzon@gmail.com>
2025-07-15 23:08:54 +0200

    chore: fix pytest path in ini function and version

--- pytorch_pca/_version.py
+++ pytorch_pca/_version.py
@@ -17,5 +17,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '0.1.1.dev2+g00dd4fb.d20250715'
-__version_tuple__ = version_tuple = (0, 1, 1, 'dev2', 'g00dd4fb.d20250715')
+__version__ = version = '0.1.1.dev0'
+__version_tuple__ = version_tuple = (0, 1, 1, 'dev0')
